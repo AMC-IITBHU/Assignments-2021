ada3e232721fafb51e3f5c4466833ce4c7eb89ce
hex-plex <sendhilsom7@gmail.com>
2021-01-20 11:41:43 +0530

    Updated testflow *Added batch file for automatic check *Test cases left empty wantedly *Need to automatic grader system for each PR

--- Week1/.test.py
+++ Week1/.test.py
@@ -1,3 +1,25 @@
 import work
 
-print(work.__dict__)
+funcs = [work.demo, work.is_palindrome, work.sqrt_of_numbers, work.Maximum, work.even_sort, work.eqn_solver]
+debug = False
+test_cases = {}     ## Will be filled after submission date
+
+score = 0
+
+for func in funcs:
+    flag = 1
+    for i,test_case in enumerate(test_cases[func.__name__]):
+        x, y = test_case
+        try:
+            assert y==func(*x)
+        except Exception as e:
+            flag = 1
+            print("function "+func.__name__+" fails on testcase "+str(i+1),end="")
+            if debug:
+                print("because ")
+                print(e)
+            else:
+                print("")
+    score += flag
+
+print("Your score is "+str(score)+"!!, You will receive extra points for smart implementation directly in the pull request")
