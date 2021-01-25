41bfb0f1b1e06a27752fb50faf64061f74bb6113
Yash Prakash <yash2002109@gmail.com>
2021-01-25 11:18:30 +0530

    Done

--- Week1/run.py
+++ Week1/run.py
@@ -7,12 +7,23 @@ Print the odd values in the given array
 '''
 arr = [5,99,36,54,88]
 ## Code Here
+for num in arr:
+    if num%2==1:
+        print(num)
+        
 
 '''
 Problem - 1
 Print all the prime numbers from 0-100
 '''
 ## Code Here
+for num in range(101):
+    count=0
+    for i in range(1,num+1):
+        if num%i==0:
+            count+=1
+    if count==2:
+        print(num)
 
 '''
 Problem - 2
@@ -20,3 +31,4 @@ Print the reverse of a string
 '''
 string = 'Reverse Me!'
 ## Code Here
+print(string[::-1])
