b951630ec0c405c27e9ff13f792f6afbabc4bdd3
AG-7219 <76441660+AG-7219@users.noreply.github.com>
2021-01-31 18:10:32 +0530

    Update work.py

--- Week1/work.py
+++ Week1/work.py
@@ -107,4 +107,6 @@ def eqn_solver(A, B, C):
     '''
 
     ## Code Here
-    return None
+    x=(C[0]*B[1]-B[0]*C[1])/(B[1]*A[0]-B[0]*A[1])
+    y=C[0]*A[1]-A[0]*C[1]/(-B[1]*A[0]+B[0]*A[1])
+    return x,y
